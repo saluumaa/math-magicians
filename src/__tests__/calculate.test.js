1f02d9fbd6aa90dbefed8abe0ea3e639e24e5a49
saluumaa <salumahassan02@gmail.com>
2023-04-05 15:04:34 +0300

    Finish calculate test

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -3,8 +3,51 @@ import calculate from "../logic/calculate";
 
 describe('calculate test', ()=> {
     test('It should clear value when click on AC', ()=> {
-        if(butttonName === 'AC'){
-            expect(total).toBe(null)
-        }
+       const obj = {
+        total: '5',
+        next: '1',
+        operation: '+',
+      };
+      const buttonName = 'AC';
+      const expected = {
+        total: null,
+        next: null,
+        operation: null,
+      };
+      const result = calculate(obj, buttonName);
+      expect(result).toEqual(expected)
     })
+
+    it('It should append a number after the operation', () => {
+        const obj = {
+            total: '5',
+            next: '1',
+            operation: '+'
+        };
+        const buttonName = '1'
+        const expected = {
+            total: '5',
+            next:'11',
+            operation: '+'
+        };
+        const result = calculate(obj, buttonName);
+        expect(result).toEqual(expected)
+    })
+
+    it('It should calculate when clicking the equal sign', () => {
+        const obj = {
+            total: '5',
+            next: '1',
+            operation: '+'
+        };
+        const buttonName = '='
+        const expected = {
+            total: '6',
+            next: null,
+            operation: null
+        };
+        const result = calculate(obj, buttonName);
+        expect(result).toEqual(expected)
+    })
+
 })
